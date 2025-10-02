9a823c48a498def4a7289b8f07086e2bc5c80af1
Bogdan Polak <bogdanpol@gmail.com>
2025-10-02 15:38:09 +0200

    Added BulletinCrawlerRequests with redirects

---
+++ HttpRequestMessageExtensions.cs
@@ -0,0 +1,138 @@
+﻿namespace CrawlerHttpCLI;
+
+#nullable enable
+
+public static class HttpRequestMessageExtensions
+{
+    private static readonly char[] PathAndQueryDelimiters
+        = ['/', '\\', ':', '?', '&', '=', '#', '%'];
+
+    private static readonly string[] GraphicsFileExtensions =
+        [
+            // image formats:
+            ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg",
+            // video formats: 
+            ".mp4", ".webm", ".ogg", ".avi", ".flv", ".mov",
+            ".wmv", ".mkv", ".mpg", ".mpeg", ".m4v", ".3gp"
+        ];
+
+    private static readonly string[] ScriptFileExtensions =
+        [
+            ".js",
+            ".ts",
+        ];
+
+    public static bool IsForGraphicalAsset(
+        this Uri? uri, out string cleanedPath)
+    {
+        cleanedPath = string.Empty;
+
+        if (uri is null) return false;
+
+        cleanedPath =
+            uri.ToString()
+            .ToLower()
+            .Trim();
+
+        if (string.IsNullOrWhiteSpace(cleanedPath))
+            return false;
+
+        return
+            PathAndQueryDelimiters.Any(cleanedPath.Contains)
+            && GraphicsFileExtensions.Any(cleanedPath.EndsWith);
+    }
+
+    public static bool IsForGraphicalAsset(this Uri? uri)
+        => uri.IsForGraphicalAsset(out _);
+
+    public static bool IsForGraphicalAsset(
+        this HttpRequestMessage? request,
+        out string cleanedPath)
+    {
+        cleanedPath = string.Empty;
+        if (request is null) return false;
+
+        return request.RequestUri
+            .IsForGraphicalAsset(out cleanedPath);
+    }
+
+    public static bool IsForGraphicalAsset(
+        this HttpRequestMessage request)
+        => request.IsForGraphicalAsset(out _);
+
+    public static bool IsForScriptAsset(
+        this HttpRequestMessage? request,
+        out string cleanedPath)
+    {
+        cleanedPath = string.Empty;
+
+        if (request is null
+            || request.Method != HttpMethod.Get
+            || request.RequestUri is null)
+        {
+            return false;
+        }
+
+        cleanedPath
+            = request.RequestUri
+            .ToString()
+            .ToLower()
+            .Trim();
+
+        return
+             ScriptFileExtensions.Any(cleanedPath.EndsWith);
+    }
+
+    public static bool IsForScriptAsset(this HttpRequestMessage request)
+        => request.IsForScriptAsset(out _);
+
+    /// <summary>
+    /// Clones a request message
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static async Task<HttpRequestMessage> CloneAsync(
+        this HttpRequestMessage request,
+        CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var clone = new HttpRequestMessage(request.Method, request.RequestUri);
+
+        try
+        {
+            foreach (var header in request.Headers)
+                clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
+
+            if (request.Content is not null)
+            {
+                var contentString = await request.Content.ReadAsStringAsync(cancellationToken);
+                if (request.Content.Headers is not null)
+                {
+                    clone.Content = new StringContent(
+                        content: contentString,
+                        encoding: System.Text.Encoding.UTF8,
+                        mediaType:
+                            request.Content.Headers.ContentType?.MediaType
+                            ?? "application/octet-stream");
+
+                    foreach (var header in request.Content.Headers)
+                        clone.Content.Headers.TryAddWithoutValidation(header.Key, header.Value);
+                }
+            }
+
+            clone.Version = request.Version;
+
+            foreach (var property in request.Options)
+                clone.Options.TryAdd(property.Key, property.Value);
+
+            return clone;
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException("Error cloning request", ex);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Text;
 using System.Text.Json;
+using System.Web;
 using AngleSharp;
 using CrawlerHttpCLI;
 
@@ -8,28 +9,61 @@ string year = "2022", modelName = "Aviator", modelId = "6807";
 
 Console.WriteLine("--------------------------------------------------------------------------");
 Console.WriteLine("[ (Ford Book Crawler) VehicleID By YearModel ]");
-var data = await CrawlerRequests.GetVehicles(modelId);
-Console.WriteLine(data);
+var json = await BookCrawlerRequests.GetVehicles(modelId);
+Console.WriteLine($"{json}\nLength = {json.Length}");
+Utils.SaveToFile(json, "01 VehicleInfo.json");
+
 
 Console.WriteLine("--------------------------------------------------------------------------");
 Console.WriteLine($"[ (Ford Book Crawler) Book for vehicleId={modelId}. {year} {modelName} ]");
-var fordBook = await CrawlerRequests.GetPublication(modelId, year, modelName);
-Console.WriteLine(JsonSerializer.Serialize(fordBook, new JsonSerializerOptions { WriteIndented = true }));
+var vehiclePublicationHtml = await BookCrawlerRequests.GetVehiclePublication(modelId, year, modelName);
+Console.WriteLine($"{vehiclePublicationHtml[ .. 500]}\nLength = {vehiclePublicationHtml.Length}");
+Utils.SaveToFile(vehiclePublicationHtml, "02 VehiclePublication.html");
 
 Console.WriteLine("--------------------------------------------------------------------------");
-Console.WriteLine($"[ (Ford Book Crawler) Table of Content ]");
-var procedureIds = await CrawlerRequests.GetProcedureList(fordBook);
-Console.WriteLine($"OemProcedureIds = [{string.Join(", ", procedureIds.Take(50))}, ...]");
-Console.WriteLine($"Length = {procedureIds.Count}");
+Console.WriteLine("[ (Ford Book Crawler) Table of Content ]");
+FordBookPub fordBook = FordBookPub.Parse(vehiclePublicationHtml);
+var url = $"{fordBook.PublicationBookTreeAndCoverBase}" +
+          $"?bookTitle={fordBook.BookTitle}&WiringBookTitle={fordBook.WiringBookTitle}";
+var command = Processing.BuildGetTableOfContentCommand(fordBook);
+var tableOfContentHtml = await BookCrawlerRequests.GetBookTableOfContent(url, command);
+Console.WriteLine($"{tableOfContentHtml[ .. 500]}\nLength = {tableOfContentHtml.Length}");
+Utils.SaveToFile(tableOfContentHtml, "03 TableOfContent.html");
+var procedureIds = await Processing.ExtractFordProcedureIds(tableOfContentHtml);
+Console.WriteLine($"OemProcedureIds = [{string.Join(", ", procedureIds.Take(50))}, ...] Length = {procedureIds.Count}");
 
 Console.WriteLine("--------------------------------------------------------------------------");
-Console.WriteLine($"[ (Ford Book Crawler) Procedure Css ]");
-var css = await CrawlerRequests.GetCssAsync(
+Console.WriteLine("[ (Ford Book Crawler) Procedure Css ]");
+var cssDesktop = await BookCrawlerRequests.GetCssAsync(
     "https://www.fordtechservice.dealerconnection.com/Content/pts.desktop.publications.css");
-Console.WriteLine(css[0 .. 200]);
-Console.WriteLine($"Length = {css.Length}");
+var cssLoader = await BookCrawlerRequests.GetCssAsync(
+    "https://www.fordtechservice.dealerconnection.com/Content/Loader.css");
+Console.WriteLine($"{cssDesktop[ .. 100]}\nLength = {cssDesktop.Length}");
+Console.WriteLine($"{cssLoader[ .. 100]}\nLength = {cssLoader.Length}");
+Utils.SaveToFile(cssDesktop, "04 pts.desktop.publications.css");
+Utils.SaveToFile(cssDesktop, "04 Loader.css");
 
-internal abstract class CrawlerRequests
+
+Console.WriteLine("\n--------------------------------------------------------------------------");
+Console.WriteLine("Bulletins");
+Console.WriteLine("--------------------------------------------------------------------------");
+Console.WriteLine("[ (Ford Bulletin Crawler) Bulletin Table of Content for Vehicle ]");
+var bulletinTocHtml = await BulletinCrawlerRequests.GetVehicleToCHtml(year, modelName, modelId);
+Console.WriteLine($"{bulletinTocHtml[ .. 500]}\nLength = {bulletinTocHtml.Length}");
+Utils.SaveToFile(bulletinTocHtml, "11 bulletinToc.html");
+var bulletinsIds = await Processing.GetBulletinsIds(bulletinTocHtml);
+Console.WriteLine($"BulletinsIds = [{string.Join(", ", bulletinsIds.Take(50))}, ...] Length = {bulletinsIds.Count}");
+
+Console.WriteLine("[ (Ford Bulletin Crawler) Bulletin Content ]");
+
+Console.WriteLine("--------------------------------------------------------------------------");
+var bulletinId = "24-7133"; // bulletinsIds[1];
+Console.WriteLine($"[ (Ford Bulletin Crawler) Bulletin Table of Content for Vehicle ] BulletinId = {bulletinId}");
+var bulletinHtml = await BulletinCrawlerRequests.GetTsbContent(bulletinId);
+Console.WriteLine($"{bulletinHtml[ .. 500]}\nLength = {bulletinHtml.Length}");
+Utils.SaveToFile(bulletinHtml, $"12 bulletin-{bulletinId}.html");
+
+internal abstract class BookCrawlerRequests
 {
     private const string FordTechServiceUrl = "https://www.fordtechservice.dealerconnection.com";
 
@@ -56,20 +90,19 @@ internal abstract class CrawlerRequests
         request.Headers.Add("Cookie", CookieProvider.Get());
         request.Version = HttpVersion.Version20;
 
-        var httpClient = new HttpClient();
+        var httpClient = new HttpClient(new HttpClientHandler{ AutomaticDecompression = DecompressionMethods.All });
         var response = await httpClient.SendAsync(request);
         response.EnsureSuccessStatusCode();
         var responseText = await response.Content.ReadAsStringAsync();
         return responseText;
     }
 
-    public static async Task<FordBookPub> GetPublication(string vehicleId, string year, string modelName)
+    public static async Task<string> GetVehiclePublication(string vehicleId, string year, string modelName)
     {
         var url = $"{FordTechServiceUrl}/publication/Book/workshop/{vehicleId}?model={modelName}&modelYear={year}";
 
         using var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-        // request.Headers.Add("Referer", FordTechServiceUrl);
         request.Headers.Add("Host", "www.fordtechservice.dealerconnection.com");
         request.Headers.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:143.0) Gecko/20100101 Firefox/143.0");
         request.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
@@ -77,13 +110,13 @@ internal abstract class CrawlerRequests
         request.Headers.Add("Accept-Encoding", "gzip, deflate, br, zstd");
         request.Headers.Add("Connection", "keep-alive");
         request.Headers.Add("Upgrade-Insecure-Requests", "1");
+        request.Headers.Add("Referer", "https://www.fordtechservice.dealerconnection.com/");
         request.Headers.Add("Sec-Fetch-Dest", "iframe");
         request.Headers.Add("Sec-Fetch-Mode", "navigate");
         request.Headers.Add("Sec-Fetch-Site", "same-origin");
         request.Headers.Add("Sec-Fetch-User", "?1");
         request.Headers.Add("Priority", "u=4");
         request.Headers.Add("TE", "trailers");
-        request.Headers.Add("Referer", "https://www.fordtechservice.dealerconnection.com/");
 
         request.Headers.Add("Cookie", CookieProvider.Get());
         request.Version = HttpVersion.Version20;
@@ -93,11 +126,10 @@ internal abstract class CrawlerRequests
         response.EnsureSuccessStatusCode();
         var html = await response.Content.ReadAsStringAsync();
 
-        var fordBookResponse = FordBookPub.Parse(html);
-        return fordBookResponse;
+        return html;
     }
 
-    private static async Task<string> GetBookTableOfContent(string url, string commandJson)
+    public static async Task<string> GetBookTableOfContent(string url, string commandJson)
     {
         using var request = new HttpRequestMessage(HttpMethod.Post, url);
         request.Headers.Add("Origin", FordTechServiceUrl);
@@ -145,11 +177,103 @@ internal abstract class CrawlerRequests
 
         return css;
     }
+}
 
-    public static async Task<List<string>> GetProcedureList(FordBookPub book)
+
+internal abstract class BulletinCrawlerRequests
+{
+    private const string FordTechServiceUrl = "https://www.fordtechservice.dealerconnection.com";
+
+    // https://www.fordtechservice.dealerconnection.com/vdirsnet/TSB_SSM/DisplayArticles.aspx
+    // ?year=2024&
+    // model=E-450&
+    // masterId=7077
+    public static async Task<string> GetVehicleToCHtml(string year, string modelName, string modelId)
     {
-        var baseUrl = book.PublicationBookTreeAndCoverBase;
-        var url = $"{baseUrl}?bookTitle={book.BookTitle}&WiringBookTitle={book.WiringBookTitle}";
+        var url = $"{FordTechServiceUrl}/vdirsnet/TSB_SSM/DisplayArticles.aspx?year={year}&model={modelName}&masterId={modelId}";
+        using var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        request.Headers.Add("Host", "www.fordtechservice.dealerconnection.com");
+        request.Headers.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:143.0) Gecko/20100101 Firefox/143.0");
+        request.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        request.Headers.Add("Accept-Language", "en-US,en;q=0.5");
+        request.Headers.Add("Accept-Encoding", "gzip, deflate, br, zstd");
+        request.Headers.Add("Connection", "keep-alive");
+        request.Headers.Add("Upgrade-Insecure-Requests", "1");
+        request.Headers.Add("Referer", "https://www.fordtechservice.dealerconnection.com/");
+        request.Headers.Add("Sec-Fetch-Dest", "iframe");
+        request.Headers.Add("Sec-Fetch-Mode", "navigate");
+        request.Headers.Add("Sec-Fetch-Site", "same-origin");
+        request.Headers.Add("Sec-Fetch-User", "?1");
+        request.Headers.Add("Priority", "u=4");
+        request.Headers.Add("TE", "trailers");
+
+        request.Headers.Add("Cookie", CookieProvider.Get());
+        request.Version = HttpVersion.Version20;
+
+        var httpClient = new HttpClient();
+        var response = await httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        var html = await response.Content.ReadAsStringAsync();
+
+        return html;
+    }
+
+    // Crawl TSBs
+    // https://www.fordtechservice.dealerconnection.com/vdirsnet/TSB_SSM/DisplayArticle.aspx
+    // ?id=24-7068
+    public static async Task<string> GetTsbContent(string tsbId)
+    {
+        var url = $"{FordTechServiceUrl}/vdirsnet/TSB_SSM/DisplayArticle.aspx?id={tsbId}";
+        using var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        request.Headers.Add("Host", "www.fordtechservice.dealerconnection.com");
+        request.Headers.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:143.0) Gecko/20100101 Firefox/143.0");
+        request.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        request.Headers.Add("Accept-Language", "en-US,en;q=0.5");
+        request.Headers.Add("Accept-Encoding", "gzip, deflate, br, zstd");
+        request.Headers.Add("Connection", "keep-alive");
+        request.Headers.Add("Upgrade-Insecure-Requests", "1");
+        request.Headers.Add("Referer", "https://www.fordtechservice.dealerconnection.com/");
+        request.Headers.Add("Sec-Fetch-Dest", "iframe");
+        request.Headers.Add("Sec-Fetch-Mode", "navigate");
+        request.Headers.Add("Sec-Fetch-Site", "same-origin");
+        request.Headers.Add("Sec-Fetch-User", "?1");
+        request.Headers.Add("Priority", "u=4");
+        request.Headers.Add("TE", "trailers");
+
+        request.Headers.Add("Cookie", CookieProvider.Get());
+        request.Version = HttpVersion.Version20;
+
+        var httpClient = new HttpClient(new HttpClientHandler{ AutomaticDecompression = DecompressionMethods.All, AllowAutoRedirect = false});
+        var response = await httpClient.SendAsync(request);
+        while (response.StatusCode == HttpStatusCode.Redirect)
+        {
+            var nextUri = response.Headers.Location;
+            if (nextUri == null)
+            {
+                throw new Exception("Location not found in response.");
+            }
+            var baseUrl = request.RequestUri?.GetLeftPart(UriPartial.Authority);
+            var url2 = nextUri.IsAbsoluteUri ? nextUri.OriginalString : baseUrl + nextUri.OriginalString;
+
+            using var nextRequest = await request.CloneAsync();
+            nextRequest.RequestUri = new Uri(url2);
+
+            response = await httpClient.SendAsync(nextRequest);
+        }
+
+        response.EnsureSuccessStatusCode();
+        var html = await response.Content.ReadAsStringAsync();
+
+        return html;
+    }
+}
+
+internal abstract class Processing
+{
+    public static string BuildGetTableOfContentCommand(FordBookPub book)
+    {
         var command = new
         {
             book.VehicleId,
@@ -172,36 +296,70 @@ internal abstract class CrawlerRequests
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase
         });
+        return json;
+    }
 
-        var content = await GetBookTableOfContent(url, json);
-
-        var config = Configuration.Default;
-        using var context = BrowsingContext.New(config);
-        using var doc = await context.OpenAsync(req => req.Content(content));
-        var wsmTree = doc.GetElementById("wsm-tree");
+    public static async Task<List<string>> ExtractFordProcedureIds(string tableOfContentHtml)
+    {
+        using var context = BrowsingContext.New(Configuration.Default);
+        using var doc1 = await context.OpenAsync(req => req.Content(tableOfContentHtml));
+        var wsmTree = doc1.GetElementById("wsm-tree");
         if (wsmTree is null)
         {
             throw new Exception("Unable to find the wsm-tree element");
         }
 
-        var procedureIds = await GetListOfProcedures($"<html>{wsmTree.OuterHtml}</html>");
-        return procedureIds;
-    }
-
-    private static async Task<List<string>> GetListOfProcedures(string tocHtml)
-    {
-        using var context = BrowsingContext.New(Configuration.Default);
-        using var doc = await context.OpenAsync(req => req.Content(tocHtml));
-
-        // start crawling procedures
-        var links = doc.QuerySelectorAll("#treeNodesDiv > .tree > .branch")
-            .Where(n => !n.QuerySelector("span")!.TextContent.Contains("quick links", StringComparison.InvariantCultureIgnoreCase))
+        var links = wsmTree.QuerySelectorAll("#treeNodesDiv > .tree > .branch")
+            .Where(n => !n.QuerySelector("span")!.TextContent.Contains("quick links",
+                StringComparison.InvariantCultureIgnoreCase))
             .SelectMany(n => n.QuerySelectorAll("a[data-for][data-procuid]"))
             .ToList();
 
         var procedureIds = links.Select(link => link.GetAttribute("data-for")).ToList();
 
         return procedureIds!;
+    }
+
+
+    public static async Task<List<string>> GetBulletinsIds(string bulletinTocHtml)
+    {
+        using var context = BrowsingContext.New(Configuration.Default);
+        using var document = await context.OpenAsync(req => req.Content(bulletinTocHtml));
+
+        var articles = document.QuerySelectorAll("#articles .article a[href]");
+        var bulletinsIds = new List<string>();
+        foreach (var article in articles)
+        {
+            var href = article.GetAttribute("href");
+            if (!string.IsNullOrEmpty(href) && href.Contains('?'))
+            {
+                var queryParamsString = href.Split('?')[1];
+                var parsed = HttpUtility.ParseQueryString(queryParamsString);
+                var tsbId = parsed["id"];
+                if (!string.IsNullOrEmpty(tsbId))
+                {
+                    bulletinsIds.Add(tsbId);
+                }
+            }
+        }
+
+        return bulletinsIds;
+    }
+}
+
+internal abstract class Utils
+{
+    private static string _outputFolder = "httpResponses";
+    public static void CleanupOutputFolder()
+    {
+        if (Directory.Exists(_outputFolder))
+            Directory.Delete(_outputFolder, true);
+    }
+
+    public static void SaveToFile(string content, string fileName)
+    {
+        Directory.CreateDirectory(_outputFolder);
+        File.WriteAllText(Path.Combine(_outputFolder,fileName) , content);
     }
 }
 
